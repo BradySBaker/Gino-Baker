d7bde0ea33576b7ff6936078b8066cab26dd5fd2
Brady Baker <bbaker@homewoodresort.com>
2026-06-30 14:14:16 -0700

    Home page improvements

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,8 +1,11 @@
 function Home() {
     return (
         <div className="home">
-            <div className="home-title">
-                <h1>Gino Baker</h1>
+            <div className="home-title-container">
+                <div className="home-title">
+                    <h1>Gino Baker</h1>
+                    <h2>Structural • TIG • MIG Welding • Fabrication</h2>
+                </div>
             </div>
             <img className="main-img" src="./pictures/front-page.png" />
         </div>
