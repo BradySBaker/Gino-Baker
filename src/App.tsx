cdc614d2f1bf7e9114b7b038c7eff54ac849bcb1
Brady Baker <bbaker@homewoodresort.com>
2026-06-30 12:18:09 -0700

    Started basic home page

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import Home from './pages/Home.tsx';
+
+function App() {
+    return (
+        <>
+            <div className="header">
+                <h1>Gino Baker</h1>
+            </div>
+            <Home />
+        </>
+    )
+}
+
+export default App;
---
+++ src/pages/Home.tsx
@@ -0,0 +1,12 @@
+function Home() {
+    return (
+        <div className="home">
+            <div className="home-title">
+                <h1>Gino Baker</h1>
+            </div>
+            <img className="main-img" src="./pictures/front-page.png" />
+        </div>
+    )
+}
+
+export default Home;
